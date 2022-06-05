f7e1bd43c0c4b31420a451a56ab1e832ccb8fc3e
Piotr Sieński <sienskipiotr2@gmail.com>
2022-06-05 23:20:28 +0200

    code done

--- main.py
+++ main.py
@@ -3,10 +3,24 @@ import random
 from pandas import read_csv
 import numpy as np
 import matplotlib.pyplot as plt
+from math import floor
 
 
+def plot_lagrange(x, y, fun, color=None):
+    xp = np.linspace(x[0], x[-1], num=1000)
+    plt.plot(xp, np.polyval(fun, xp), c=color, linewidth=3, label="interpolated curve - lagrange")
+    plt.scatter(x, y, c='#000000', marker='x', s=100, label="Points for interpolation")
 
-def lagrange(x, y):
+
+def plot_splines(x, y, k, coeffs, color=None):
+    for i in range(k):
+        xp = np.linspace(x[i], x[i + 1], num=10)
+        coeff = coeffs[(4 * i):4 * (i + 1)]
+        plt.plot(xp, np.polyval(coeff, xp), c=color, linewidth=3, label="interpolated curve - splines")
+    plt.scatter(x, y, c='#000000', marker='x', s=100, label="Points for interpolation")
+
+
+def lagrange(x, y, plot=False):
     fun = np.zeros(x.shape[0])
     for point in range(x.shape[0]):
         xs = np.append(x[:point], x[point+1:])
@@ -14,16 +28,19 @@ def lagrange(x, y):
         denominator = np.prod(x[point] - xs)
         fun += y[point] * (numerator / denominator)
 
-    xp = np.linspace(x[0], x[-1], num=1000)
-    plt.plot(xp, np.polyval(fun, xp))
-    plt.scatter(x, y)
-    plt.ylim([-50, max(y) + 50])
-    plt.xlabel("x [m]")
-    plt.ylabel("y [m]")
-    plt.title(f"Interpolacja Lagrange dla {x.shape[0] - 1} przedziałów")
-    plt.show()
+    if plot:
+        plot_lagrange(x, y, fun)
+        plt.ylim([min(y) - 50, max(y) + 50])
+        plt.xlabel("x [m]")
+        plt.ylabel("y [m]")
+        plt.title(f"Interpolacja Lagrange dla {x.shape[0] - 1} przedziałów")
 
-def splines(x, y):
+        plt.show()
+
+    return fun
+
+
+def splines(x, y, plot=False):
     k = x.shape[0] - 1
     A = np.zeros((4*k, 4*k))
     r = np.zeros((4*k, 1))
@@ -44,20 +61,24 @@ def splines(x, y):
         r[(4*i)+3] = 0
     coeffs = solve(A, r)
 
-    for i in range(k):
-        xp = np.linspace(x[i], x[i+1], num=10)
-        coeff = coeffs[(4*i):4*(i+1)]
-        plt.plot(xp, np.polyval(coeff, xp))
-    plt.scatter(x, y)
-    plt.ylim([-50, max(y) + 50])
-    plt.xlabel("x [m]")
-    plt.ylabel("y [m]")
-    plt.title(f"Interpolacja Spline dla {k} przedziałów")
-    plt.show()
+    if plot:
+        plot_splines(x, y, k, coeffs)
+        plt.ylim([min(y) - 50, max(y) + 50])
+        plt.xlabel("x [m]")
+        plt.ylabel("y [m]")
+        plt.title(f"Interpolacja Spline dla {k} przedziałów")
+        plt.show()
+
+    return coeffs
 
 
+def interpolate(x, y, k, interval="equal", method="both", plotting="none", add_txt=""):
 
-def interpolate(x, y, k, interval="equal"):
+    if method != 'lagrange' and method != 'spline':
+        method = "both"
+    if plotting != 'individual' and plotting != 'one' and plotting != 'both':
+        plotting = "none"
+
     if interval == "random":
         idx = random.sample(range(1, x.shape[0]-1), k-1)
         idx += [0, x.shape[0]-1]
@@ -67,14 +88,42 @@ def interpolate(x, y, k, interval="equal"):
     else:
         if interval != "equal":
             print("choosing equal interval size")
-        w = x.shape[0] // k
+        w = floor((x.shape[0] - 1) / k)
         if (x.shape[0]-1) % k != 0:
             print(f"cant equally divide x in {k} intervals")
 
         x_sampled = x[::w]
         y_sampled = y[::w]
-    lagrange(x_sampled, y_sampled)
-    splines(x_sampled, y_sampled)
+    cl, cs = None, None
+
+    if plotting == "individual" or plotting == "both":
+        plot = True
+    else:
+        plot = False
+
+    if method == "lagrange" or method == "both":
+        cl = lagrange(x_sampled, y_sampled, plot)
+    if method == "spline" or method == "both":
+        cs = splines(x_sampled, y_sampled, plot)
+
+    if plotting == "one" or plotting == "both":
+        plt.scatter(x, y, c='#ff0000', label="all datapoints")
+        if cl is not None:
+            plot_lagrange(x_sampled, y_sampled, cl, color='#00ff00')
+        if cs is not None:
+            plot_splines(x_sampled, y_sampled, k, cs, color='#0000ff')
+        # remove repeating labels
+        handles, labels = plt.gca().get_legend_handles_labels()
+        by_label = dict(zip(labels, handles))
+        plt.legend(by_label.values(), by_label.keys())
+
+        plt.ylim([min(y) - 50, max(y) + 50])
+        plt.xlabel("x [m]")
+        plt.ylabel("y [m]")
+        plt.title(f"Interpolacje Lagrange i Spline dla {k} przedziałów" + " " + add_txt)
+
+        plt.show()
+
 
 def solve(A, B):
     n = A.shape[0]
@@ -112,10 +161,23 @@ def solve(A, B):
     return ret
 
 
+def load_data(path):
+    df = read_csv(path)
+    return df.iloc[:, 0].to_numpy(), df.iloc[:, 1].to_numpy()
+
+
 def main():
-    df = read_csv("100.csv")
-    x, y = df.iloc[:, 0].to_numpy(), df.iloc[:, 1].to_numpy()
-    interpolate(x, y, 15, "equal")
+    x, y = load_data("WielkiKanionKolorado.csv")
+    interpolate(x, y, 5, interval="equal", method="both", plotting="one", add_txt="WielkiKanionKolorado")
+    x, y = load_data("Obiadek.csv")
+    interpolate(x, y, 5, interval="equal", method="both", plotting="one", add_txt="Obiadek")
+    x, y = load_data("MountEverest.csv")
+    interpolate(x, y, 5, interval="equal", method="both", plotting="one", add_txt="MountEverest")
+    x, y = load_data("100.csv")
+    interpolate(x, y, 5, interval="equal", method="both", plotting="one", add_txt="")
+    interpolate(x, y, 20, interval="equal", method="both", plotting="individual", add_txt="")
+    interpolate(x, y, 20, interval="random", method="both", plotting="both", add_txt="")
+
 
 if __name__ == "__main__":
     main()
